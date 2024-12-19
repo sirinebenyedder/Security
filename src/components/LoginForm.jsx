6e15b68fd28c3538c1ebe9f2c545fc1ee76a22a3
sirinebenyedder <sirineyedder0@gmail.com>
2024-12-19 23:55:19 +0100

    done

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -60,7 +60,7 @@ export default function LoginForm() {
           )}
 
           <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don't have an account? <span className="underline">Register</span>
+          Don&apos;t have an account? <span className="underline">Register</span>
           </Link>
 
           <div className="flex justify-center items-center">
